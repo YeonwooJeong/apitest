c70d525db0a449cf72a989f4920182b1d4c172f6
yeonwoojeong <todriver08@nate.com>
2017-03-13 17:24:40 +0900

    java.lang.SecurityException: class "org.hamcrest.Matchers"'s signer information 오류 수정

--- src/test/java/NaverAPITest/NAPITest/test.java
+++ src/test/java/NaverAPITest/NAPITest/test.java
@@ -23,18 +23,17 @@ public class test {
 		try {
 			String text = URLEncoder.encode("최순실", "UTF-8");
 			given().
+			  header("X-Naver-Client-Id", "wRA7VbWi9bfyoszVUBF0").
+			  header("X-Naver-Client-Secret","gyuAOlFhfB").
 			 params( //URL 주소의 파라미터 값을 넣어줌
-			 "query",text,
-			 "key","wRA7VbWi9bfyoszVUBF0",
-			 "sort","sim",
-			 "display","10",
-			 "start","1"		 
+			 "query",text	 
 			 ).when().
 			
 			 /*baseURL 을 제외한 뒷부분 주소를 get하고
 			 *body에서 해당 title 내용을 가져올 위치 지정 후
 			 *확인할 값을 equalTo에 넣어 확인*/
-			 get("/v1/search/news").then().toString();
+			 get("/v1/search/news.xml").then().body("rss.channel.title", equalTo("Naver Open API - news ::'최순실'"));
+//			 get("/v1/search/news.xml").then().toString();
 			
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
